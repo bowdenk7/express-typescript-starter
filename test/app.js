8db854f9d7630fdc948f43d929b4b4363ee77b59
Bowden Kelly <wilkelly@microsoft.com>
2017-04-10 14:57:31 -0700

    removed yarn, fixed tests

--- test/app.js
+++ test/app.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app.js');
+const app = require('../app/server.js');
 
 describe('GET /', () => {
   it('should return 200 OK', (done) => {
