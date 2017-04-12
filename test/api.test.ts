2a9f617e166057646d854813bbb188b3324bfa37
Bowden Kelly <wilkelly@microsoft.com>
2017-04-11 17:37:44 -0700

    added force exit to jest, couldn't get ts tests to work

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,9 +1,6 @@
 // import * as request from "supertest";
 // import {app} from "../src/server";
 
-// import * as request from "supertest";
-// import {default as app} from "../src/server";
-
 let request = require("supertest");
 let app = require("../src/server");
 
