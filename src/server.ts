d6d76edae4ea73433a106917ff53feb1e79336d9
Bowden Kelly <wilkelly@microsoft.com>
2017-04-13 13:33:33 -0700

    downgraded jquery and removed linked accounts from profile view

--- src/server.ts
+++ src/server.ts
@@ -135,14 +135,6 @@ app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "
 app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
   res.redirect(req.session.returnTo || "/");
 });
-app.get("/auth/google", passport.authenticate("google", { scope: "profile email" }));
-app.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "/login" }), (req, res) => {
-  res.redirect(req.session.returnTo || "/");
-});
-app.get("/auth/twitter", passport.authenticate("twitter"));
-app.get("/auth/twitter/callback", passport.authenticate("twitter", { failureRedirect: "/login" }), (req, res) => {
-  res.redirect(req.session.returnTo || "/");
-});
 
 
 /**
